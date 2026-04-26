d001ea1211e67ca689a411d392e47f76fc918610
Gabriel Henrique <henriquedeveloper24@gmail.com>
2026-04-26 16:37:06 -0300

    feat: feito a tela principal com a listagem dos veículos, informações e header com data atual

--- mobile/MotorizApp/App.tsx
+++ mobile/MotorizApp/App.tsx
@@ -4,13 +4,9 @@
  *
  * @format
  */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { StatusBar, useColorScheme } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { DashboardVeiculos } from './src/screens/DashboardVeiculos';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -18,28 +14,10 @@ function App() {
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <DashboardVeiculos />
     </SafeAreaProvider>
   );
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
